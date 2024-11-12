e7928633981a7ec3a077f39623edd89eee1daddc
Qusay <qusaymheo1997@gmail.com>
2024-11-12 12:43:17 +0300

    Portfolio

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,55 +7,51 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 function Hero() {
   return (
- <>
-
- <div className="container-fluid hero-section" id="home">
-      
-      <div className="hero-content">
-        <div
-        className="row">
-          <motion.div
-        variants={fadeIn("right", 0.2)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.7 }}  className="col-md-6 col-sm-12 ">
-            <div
-             
-              className="img-box"
+    <>
+      <div className="container-fluid hero-section" id="home">
+        <div className="hero-content">
+          <div className="row">
+            <motion.div
+              variants={fadeIn("right", 0.2)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="col-md-6 col-sm-12 "
             >
-              <div className="d-flex justify-content-center align-items-center">
-                <img
-                  className="img-fluid"
-                  src={QusayImg}
-                  alt="Personal Photo"
-                />
+              <div className="img-box">
+                <div className="d-flex justify-content-center align-items-center">
+                  <img
+                    className="img-fluid"
+                    src={QusayImg}
+                    alt="Personal Photo"
+                  />
+                </div>
               </div>
-            </div>
-          </motion.div>
-          {/* Secondcol */}
-          <motion.div
-        variants={fadeIn("left", 0.2)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.7 }}  className="col-md-5 col-sm-12 col-xs-12 self-introduction p-5">
-            Hi There, I’m Qusay from Syria.
-            <br />
-            I'm working as a Front-End Web Developer since 2 years. <br /> I
-            have developed many websites and I have been a part of some very
-            good web design projects at Darrebni Company .
-            <br />
-            My skills are HTML5, CSS3, Bootstrap, Javascript and React JS.{" "}
-            <br />
-            You can see my projects on my GitHub account or my YouTube Channel
-            (Code With B11) . I'm looking forward to be a Full Stack Web
-            Developer.
-          </motion.div>
+            </motion.div>
+            {/* Secondcol */}
+            <motion.div
+              variants={fadeIn("left", 0.2)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="col-md-5 col-sm-12 col-xs-12 self-introduction p-5"
+            >
+              Hi There, I’m Qusay from Syria.
+              <br />
+              I'm working as a Web Developer since 2 years. <br /> I have
+              developed many websites and I have been a part of some very good
+              web design projects at Darrebni Company .
+              <br />
+              My skills are HTML5, CSS3, Bootstrap, Javascript , PHP/Laravel and
+              React JS. <br />
+              You can see my projects on my GitHub account or my YouTube Channel
+              (Code With B11) . I'm looking forward to be a Full Stack Web
+              Developer.
+            </motion.div>
+          </div>
         </div>
       </div>
-        
-      
-    </div>
- </>
+    </>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,49 +2,44 @@ import React from "react";
 import "./Navbar.css";
 function Navbar() {
   return (
-    <div className="nav-content fixed-top navbar navbar-expand-lg ">
-     
-        <a className="logo" href="#home">
-          B11
-        </a>
+    <div className="navbar navbar-expand-lg ">
+      <a className="logo" href="#home">
+        B11
+      </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse p-2"
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact-me">Contact</a>
-            </li>
-          </ul>
-          <div className="call-icon-box">
-            <i className="call-icon bi bi-telephone-plus-fill me-3"></i>
-            <div className="call-icon-phone-number" title="Call Me">
-              0993519890
-            </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse p-2" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact-me">Contact</a>
+          </li>
+        </ul>
+        <div className="call-icon-box">
+          <i className="call-icon bi bi-telephone-plus-fill me-3"></i>
+          <div className="call-icon-phone-number" title="Call Me">
+            0993519890
           </div>
         </div>
-      
+      </div>
     </div>
   );
 }
